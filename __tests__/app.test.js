69d2ccf427aac4adaeaf297562fbe0dfbe53b7da
Dave Fisher <dave.j.fish@gmail.com>
2022-07-25 17:41:37 -0700

    playing with test cases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,22 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
+// const pool = require('../lib/utils/pool');
+// const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
 const { signs } = require('../data/zodiac');
 
 describe('zodiac sign routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+  // beforeEach(() => {
+  //   return setup(pool);
+  // });
 
   it('example test - delete me!', () => {
     expect(1).toEqual(1);
   });
-  afterAll(() => {
-    pool.end();
-  });
+
+  // afterAll(() => {
+  //   pool.end();
+  // });
 });
 
 it('/zodiac returns id and name for each sign', async () => {
