ce4aaaf99305bd281c38dab2c167ce75e2ce9a85
Dave Fisher <dave.j.fish@gmail.com>
2022-07-26 14:47:41 -0700

    test working... maybe?

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,22 +1,10 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
+// const pool = require('../lib/utils/pool');
+// const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const deepEqual = require('deep-equal');
 
 const { signs } = require('../data/zodiac');
-
-describe('zodiac sign routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
-  afterAll(() => {
-    pool.end();
-  });
-});
 
 it('/zodiac returns id and name for each sign', async () => {
   const res = await request(app).get('/zodiac');
@@ -37,4 +25,16 @@ it('/zodiac/id returns detail about specific sign', async () => {
   };
 
   expect(res.body).toEqual(expected);
+});
+
+it('/zodiac/horoscopes/sign returns horoscope for that sign', async () => {
+  const leo = await fetch('https://ohmanda.com/api/horoscope/leo/');
+  const expected = {
+    sign: 'leo',
+    date: '2022-07-26',
+    horoscope:
+      'A cosmic alliance between the Cancer moon and Venus can help you reconnect with your inner strength this morning, dear Lion, but only if you\'re willing to show yourself plenty of love and compassion. Take a moment to fully appreciate your current situation and resilience up until this point, as you may need to rely on these sentiments in order to push through the day. Tensions will brew between Mercury and Mars, threatening to throw you off course with challenges and barriers to your path. Luckily, you\'ll have an opportunity to regain control later this evening when Luna blows a kiss to passionate Mars.',
+  };
+  
+  deepEqual(leo.json(), expected);
 });
