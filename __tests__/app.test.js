1e3ef7b6ec0540d569e84973b6fe19b6779e22c0
Dave Fisher <dave.j.fish@gmail.com>
2022-07-26 15:04:36 -0700

    updated tests to work, updated horoscopes to give all info not just horoscope

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@
 // const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const deepEqual = require('deep-equal');
 
 const { signs } = require('../data/zodiac');
 
@@ -27,14 +26,14 @@ it('/zodiac/id returns detail about specific sign', async () => {
   expect(res.body).toEqual(expected);
 });
 
-it('/zodiac/horoscopes/sign returns horoscope for that sign', async () => {
-  const leo = await fetch('https://ohmanda.com/api/horoscope/leo/');
+it('/zodiac/horoscopes/sign returns the horoscope for the matching sign parameter', async () => {
+  const leo = await request(app).get('/zodiac/horoscopes/leo');
   const expected = {
     sign: 'leo',
     date: '2022-07-26',
     horoscope:
       'A cosmic alliance between the Cancer moon and Venus can help you reconnect with your inner strength this morning, dear Lion, but only if you\'re willing to show yourself plenty of love and compassion. Take a moment to fully appreciate your current situation and resilience up until this point, as you may need to rely on these sentiments in order to push through the day. Tensions will brew between Mercury and Mars, threatening to throw you off course with challenges and barriers to your path. Luckily, you\'ll have an opportunity to regain control later this evening when Luna blows a kiss to passionate Mars.',
   };
-  
-  deepEqual(leo.json(), expected);
+
+  expect(leo.body).toEqual(expected);
 });
